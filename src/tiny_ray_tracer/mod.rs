7a3dc27664f9c62aa37a1427e6166f2301d52490
Neil <wrz750726@gmail.com>
2025-02-27 23:03:28 -0800

    feat: Begin tiny render course!

--- src/tiny_ray_tracer/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod step_01_draw_scene;
-pub mod step_02_one_sphere;
-pub mod step_03_spheres;
-pub mod step_04_lighting;
-pub mod step_05_specular_lighting;
-
-pub use step_02_one_sphere::pixel_to_world;
-pub use step_03_spheres::{BACKGROUND_COLOR, FOV, Z};
